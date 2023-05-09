8b743b910d058df74ad1bef11e2cdc3bc53c39c2
Toe Tet Aung <toetet1997aung@gmail.com>
2023-05-09 16:23:38 +0630

    error fixing and finishing project

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || process.argv[2] || 3000;
-
-//!Obj
-let blogObj = [];
+const _ = require('lodash');
 
 //!middleware section
 app.use(express.static('./public'));
@@ -13,46 +11,68 @@ app.set('view engine' , 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use((req,res,next) => {
-    // console.log(req.method);
+    console.log(req.method);
     // console.log(req.body);
     next();
 });
 
+//!Global Obj
+let posts = [];
 
 //!method get section
 
 app.get('/', (req,res) => {
-    res.render('./home.ejs', {'titleHead': 'Home', blogObj});
+    res.render('./home.ejs', {titleHead: 'Home', posts});
 });
 
 app.get('/blog', (req,res) => {
-    res.render('./blog.ejs', {'titleHead': 'Blog'});
+    res.render('./blog.ejs', {titleHead: 'Blog'});
 });
 
 app.get('/contact', (req,res) => {
-    res.render('./contact.ejs', {'titleHead': 'Contact'});
+    res.render('./contact.ejs', {titleHead: 'Contact'});
 });
 
 app.get('/about', (req,res) => {
-    res.render('./about.ejs', {'titleHead': 'About'});
+    res.render('./about.ejs', {titleHead: 'About'});
 });
 
-app.get('/:id',(req,res) => {
-    let id = req.params.id;
-    let blogItem = blogObj.find((blog) => blog.id == id);
-    res.render('./blogItem.ejs', {'titleHead': blogItem.blogTitle, blogItem});
+
+
+app.get('/posts/:postTitle',(req,res) => {
+    const requestTitle = _.lowerCase(req.params.postTitle);
+    
+    posts.forEach(post => {
+        const storedTitle = _.lowerCase(post.title);
+        if( storedTitle === requestTitle){
+            res.render('./postItem.ejs', {
+                titleHead : post.title, 
+                title : post.title,
+                content : post.content
+            });
+        };
+    });
+
 });
 
 
 //!method post section
 
-app.post('/blog', (req,res) =>{
-    const newBlog = req.body;
-    newBlog.id = blogObj.length +1
-    blogObj.push(newBlog);
-    res.render('./home.ejs', {'titleHead': 'Home', blogObj});
-});
+// app.post('/blog', (req,res) =>{
+//     const newBlog = req.body;
+//     newBlog.id = posts.length +1
+//     posts.push(newBlog);
+//     res.render('./home.ejs', {'titleHead': 'Home', posts});
+// });
 
+app.post('/blog', (req,res) => {
+    const newPost = {
+        title : req.body.blogTitle,
+        content : req.body.blogBody
+    };
+    posts.push(newPost);
+    res.redirect('/');
+});
 
 
 
@@ -63,5 +83,5 @@ app.listen(port, () => {
 
 
 app.use((req,res) => {
-    res.render('./error.ejs', {'titleHead': 'Error 404'});
+    res.render('./error.ejs', {titleHead : 'Error 404'});
 });
